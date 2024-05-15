fb3d1cbdfd90c5d1260b72feeaa355c6f41c85c0
Yulia Tran <alwaysonline724@gmail.com>
2024-05-15 15:32:53 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -34,3 +34,49 @@ for (let i=0; i < skills.length; i++){
 skill.innerText = skills[i];
 skillsList.appendChild(skill);
 }
+//message forms
+const messageForm = document.forms.leave_message;
+
+messageForm.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log('Name:', usersName);
+    console.log('Email:', usersEmail);
+    console.log('Message:', usersMessage);
+
+    ////
+
+ const messageSection = document.getElementById('messages');
+ const messageList = messageSection.querySelector('ul');
+ const newMessage = document.createElement('li');
+
+ newMessage.innerHTML = `
+     <a href="mailto:${usersEmail}">${usersName}</a>: 
+     <span>${usersMessage}</span>`;
+
+ const removeButton = document.createElement('button');
+
+ removeButton.innerText = 'remove';
+ removeButton.type = 'button';
+
+
+ removeButton.addEventListener('click', function() {
+
+     const entry = removeButton.parentNode;
+     entry.remove();
+ });
+
+
+ newMessage.appendChild(removeButton);
+ messageList.appendChild(newMessage);
+
+    ////
+
+     event.target.reset();
+
+});
